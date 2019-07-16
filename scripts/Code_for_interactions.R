f479fcc1150e4d36fd3c7d23e8888200725e0751
beckybanbury <43652200+beckybanbury@users.noreply.github.com>
2019-07-16 10:52:36 -0400

    Update Code_for_interactions.R

--- scripts/Code_for_interactions.R
+++ scripts/Code_for_interactions.R
@@ -55,7 +55,7 @@ dist.to.keep <- ForC_simplified$managed %in% 0 & ForC_simplified$disturbed %in% 
 min.dbh.to.keep <- ForC_simplified$min.dbh <= 10 ##& !is.na(ForC_simplified$min.dbh) # this doesn work great, not enough data, but maybe we can just keep the NAs?
 min.dbh.to.keep <- rep(TRUE, nrow(ForC_simplified))
 
-ForC_simplified <- ForC_simplified[rows.with.response & ages.to.keep & fixed.no.na.1 & fixed.no.na.2, ]
+ForC_simplified <- ForC_simplified[dist.to.keep, ]
 
 ## give leaf type
 broadleaf_codes <- c("2TEB", "2TDB", "2TB")
@@ -86,40 +86,32 @@ all.response.variables <- gsub("(_0|_1|_2|_3|_4|_5)", "", all.response.variables
 all.response.variables <- all.response.variables[all.response.variables %in% ForC_simplified$variable.name]
 all.response.variables <- unique(gsub("_\\d", "", all.response.variables))
 
-response.variables.groups <- list(c("GPP", "NPP", "BNPP_root", "BNPP_root_fine"),
-                                  c("ANPP_1", "ANPP_foliage", "ANPP_repro"),
-                                  c("ANPP_woody", "ANPP_woody_stem", "ANPP_woody_branch"),
-                                  c("woody.mortality_ag", "R_auto"))
 
 all.response.variables[!all.response.variables %in% unlist(response.variables.groups)]
 
-## fixed variables list ####
-fixed.variables <- c("mat", "map", "lat", "AnnualMeanTemp", "MeanDiurnalRange", "TempSeasonality", "TempRangeAnnual", "AnnualPre", "PreSeasonality", "CloudCover", "AnnualFrostDays", "AnnualPET", "AnnualWetDays", "VapourPressure", "SolarRadiation", "Aridity", "PotentialEvapotranspiration", "VapourPressureDeficit")
 
 ## prepare results table
 
 all.results <- NULL
 
 
-
 library(AICcmodavg)
 all.results <- NULL
 all.aictab <- NULL
 
-fixed.variables.set1 <- c("mat", "map", "lat", "AnnualMeanTemp", "TempSeasonality", "TempRangeAnnual", "AnnualPre", "AnnualPET", "AnnualWetDays", "VapourPressure", "SolarRadiation", "Aridity", "PotentialEvapotranspiration", "VapourPressureDeficit")
-fixed.variables.set2 <- c("mat", "map", "lat", "AnnualMeanTemp", "TempSeasonality", "TempRangeAnnual", "AnnualPre", "AnnualPET", "AnnualWetDays", "VapourPressure", "SolarRadiation", "Aridity", "PotentialEvapotranspiration", "VapourPressureDeficit")
+fixed.variables.set1 <- c("mat", "map", "TempSeasonality", "AnnualWetDays", "SolarRadiation", "Aridity", "PotentialEvapotranspiration", "VapourPressureDeficit")
+fixed.variables.set2 <- c("mat", "map", "TempSeasonality", "AnnualWetDays", "SolarRadiation", "Aridity", "PotentialEvapotranspiration", "VapourPressureDeficit")
 
 
 response.variables.groups <- list(c("GPP", "NPP", "BNPP_root", "BNPP_root_fine"),
-                                  c("ANPP_1", "ANPP_foliage"),
-                                  c("ANPP_woody", "ANPP_woody_stem", "ANPP_woody_branch"))
+                                  c("ANPP", "ANPP_foliage", "ANPP_woody_stem"),
+                                  c("R_auto", "R_auto_root"))
 
 for(response.variables in response.variables.groups){
   
   if(response.variables[1] == "GPP") n <- 1
-  if(response.variables[1] == "ANPP_1") n <- 2
-  if(response.variables[1] == "ANPP_woody") n <- 3
-  if(response.variables[1] == "woody.mortality_ag") n <- 4
+  if(response.variables[1] == "ANPP") n <- 2
+  if(response.variables[1] == "R_auto") n <- 3
   
   ### mature forests only ####
   for (age in ages){
@@ -147,8 +139,8 @@ for(response.variables in response.variables.groups){
       
       for (response.v in response.variables){
         
-        if(response.v %in% "NPP") responses.to.keep  <- c("NPP_1")
-        if(response.v %in% "ANPP") responses.to.keep  <- c("ANPP_1")
+        if(response.v %in% "NPP") responses.to.keep  <- c("NPP_1", "NPP_2")
+        if(response.v %in% "ANPP") responses.to.keep  <- c("ANPP_1", "ANPP_2")
         if(response.v %in% "ANPP_litterfall") responses.to.keep  <- c("ANPP_litterfall_1")
         if(!response.v %in% c("NPP", "ANPP", "ANPP_litterfall")) responses.to.keep  <- response.v
         
@@ -169,79 +161,47 @@ for(response.variables in response.variables.groups){
         
         mod <-  lmer(mean ~ 1 + (1|geographic.area/plot.name), data = df, REML = F)
         mod.linear <- lmer(mean ~ poly(fixed1, 1) + masl + (1|geographic.area/plot.name), data = df, REML = F)
-        mod.s.int <- lmer(mean ~ fixed1*masl + (1|geographic.area/plot.name), data = df, REML = F)
         mod.poly <- lmer(mean ~ poly(fixed1, 2) + masl + (1|geographic.area/plot.name), data = df, REML = F)
         mod.int <- lmer(mean ~ fixed1*fixed2 + masl + (1|geographic.area/plot.name), data = df, REML = F)
-        mod.d.int <- lmer(mean ~ fixed1*fixed2 + fixed1*masl + (1|geographic.area/plot.name), data = df, REML = F)
+        mod.add <- lmer(mean ~ fixed1 + fixed2 + masl + (1|geographic.area/plot.name), data = df, REML = F)
         
-        # mod.age.linear <- lmer(mean ~ fixed + stand.age + (1|geographic.area/plot.name), data = df)
-        # mod.age.int <- lmer(mean ~ fixed * stand.age + (1|geographic.area/plot.name), data = df)
-        # mod.age.linear.poly <- lmer(mean ~ poly(fixed, 2) + stand.age + (1|geographic.area/plot.name), data = df)
-        # mod.age.int.poly <- lmer(mean ~ poly(fixed, 2) * stand.age + (1|geographic.area/plot.name), data = df)
-        # 
-        # 
-        # aictab <- aictab(list(mod.linear = mod.linear, mod.poly = mod.poly, mod.age.linear = mod.age.linear, mod.age.int = mod.age.int, mod.age.linear.poly = mod.age.linear.poly, mod.age.int.poly = mod.age.int.poly), sort = T)
         
-        best.model <- as.character(aictab(list(mod = mod, mod.linear = mod.linear, mod.poly = mod.poly, mod.int = mod.int, mod.d.int = mod.d.int, mod.s.int = mod.s.int), sort = T)$Modname[1])
-        delta.aic <- as.numeric(aictab(list(mod.linear = mod.linear, mod.poly = mod.poly, mod.int = mod.int, mod.d.int = mod.d.int, mod.s.int = mod.s.int), sort = T)$Delta_AICc[2])
+        best.model <- as.character(aictab(list(mod = mod, mod.linear = mod.linear, mod.poly = mod.poly, mod.int = mod.int, mod.add = mod.add), sort = T)$Modname[1])
+        delta.aic <- as.numeric(aictab(list(mod.linear = mod.linear, mod.poly = mod.poly, mod.int = mod.int, mod.add = mod.add), sort = T)$Delta_AICc[2])
         delta.aic <- signif(delta.aic, digits=4)
         
         if (best.model == "mod.poly") mod.full <- lmer(mean ~ poly(fixed1, 2) + masl + (1|geographic.area/plot.name), data = df, REML = F)
         if (best.model == "mod.linear") mod.full <- lmer(mean ~ poly(fixed1, 1) + masl + (1|geographic.area/plot.name), data = df, REML = F)
         if (best.model == "mod") mod.full <- lmer(mean ~ poly(fixed1, 1) + masl + (1|geographic.area/plot.name), data = df, REML = F)
         if (best.model == "mod.int") mod.full <- lmer(mean ~ fixed1*fixed2 + masl + (1|geographic.area/plot.name), data = df, REML = F)
-        if(best.model == "mod.d.int") mod.full <- lmer(mean ~ fixed1*fixed2 + fixed1*masl + (1|geographic.area/plot.name), data = df, REML = F)
-        if(best.model == "mod.s.int") mod.full <- lmer(mean ~ fixed1*masl + (1|geographic.area/plot.name), data = df, REML = F)
-        # if (best.model == "mod.age.linear") mod.full <- lmer(mean ~ fixed + stand.age + (1|geographic.area/plot.name), data = df, REML = F)
-        # if (best.model == "mod.age.int") mod.full <- lmer(mean ~ fixed * stand.age + (1|geographic.area/plot.name), data = df, REML = F)
-        # if (best.model == "mod.age.linear.poly") mod.full <- lmer(mean ~ poly(fixed, 2) + stand.age + (1|geographic.area/plot.name), data = df, REML = F)
-        # if (best.model == "mod.age.int.poly") mod.full <- lmer(mean ~ poly(fixed, 2) * stand.age + (1|geographic.area/plot.name), data = df, REML = F)
+        if (best.model == "mod.add") mod.full <- lmer(mean ~ fixed1 + fixed2 + masl + (1|geographic.area/plot.name), data = df, REML = F)
         
-        
-        significant.effect <- anova(mod, mod.full)$"Pr(>Chisq)"[2] < 0.05
+         significant.effect <- anova(mod, mod.full)$"Pr(>Chisq)"[2] < 0.05
         significance <- anova(mod, mod.full)$"Pr(>Chisq)"[2]
         sample.size <- length(df$mean)
         
-        # if (best.model == "mod.poly") mod.full <- lmer(mean ~ poly(fixed, 2) + (1|geographic.area/plot.name), data = df, REML = T)
-        # if (best.model == "mod.linear") mod.full <- lmer(mean ~ poly(fixed, 1) + (1|geographic.area/plot.name), data = df, REML = T)
-        # 
-        # newDat <- expand.grid(fixed = seq(min(df$fixed), max(df$fixed), length.out = 100))
-        # 
-        # newDat$fit <- predict(mod.full, newDat, re.form = NA)
-        # 
-        # if(first.plot) plot(mean ~ fixed, data = df, xlab = "", ylab = "", col = response.v.color, ylim = ylim, xaxt = "n", yaxt = "n")
-        # if(!first.plot) points(mean ~ fixed, data = df, ylab = "", col = response.v.color) 
-        # lines(fit ~ fixed, data = newDat, col = response.v.color, lty = ifelse(significant.effect, 1, 2))
+        if (best.model == "mod.poly") mod.full <- lmer(mean ~ poly(fixed1, 2) + masl + (1|geographic.area/plot.name), data = df, REML = T)
+        if (best.model == "mod.linear") mod.full <- lmer(mean ~ poly(fixed1, 1) + masl + (1|geographic.area/plot.name), data = df, REML = T)
+        if (best.model == "mod") mod.full <- lmer(mean ~ poly(fixed1, 1) + masl + (1|geographic.area/plot.name), data = df, REML = T)
+        if (best.model == "mod.int") mod.full <- lmer(mean ~ fixed1*fixed2 + masl + (1|geographic.area/plot.name), data = df, REML = T)
+        if (best.model == "mod.add") mod.full <- lmer(mean ~ fixed1 + fixed2 + masl + (1|geographic.area/plot.name), data = df, REML = T)
         
-        # if (best.model == "mod.poly") lines(fit ~ fixed, data = newDat, col = response.v.color, lty = ifelse(significant.effect, 1, 2))
-        # if (best.model == "mod.linear") abline(fixef(mod.linear), col = response.v.color, lty = ifelse(significant.effect, 1, 2)) 
-        
-        # first.plot <- FALSE
-        
+               
         r <- round(fixef(mod.full), 2)
         equation <-  paste(response.v, "=", r[1], "+", fixed.v1,  "x", r[2])
-        # legend <- paste(response.v, "sample size =", sample.size)
-        # mtext(side = 3, line = -which(response.variables %in% response.v), text = legend, adj = 0.1, col = response.v.color, cex = 0.5)
-        
+       
         significance <- anova(mod, mod.full)$"Pr(>Chisq)"[2]
         significance <- signif(significance, digits=4)
         
         Rsq <- as.data.frame(r.squaredGLMM(mod.full))
         Rsq <- signif(Rsq, digits=4)
-        # legend2 <- paste(response.v, "r-squared = ", Rsq[1], "p-value = ", significance)
-        # mtext(side = 3, line = -which(response.variables %in% response.v), text = legend2, adj = 0.9, col = response.v.color, cex = 0.5)
+        
         results <- data.frame(response = response.v, fixed1 = fixed.v1, fixed2 = fixed.v2, random = "geographic.area/plot.name", Age.filter = age, best.model = best.model, significant = significant.effect, p.value = significance, sample.size = sample.size, Rsq = Rsq, delta.aic = delta.aic)
         
         all.results <- rbind(all.results, results)
-        # all.aictab <- rbind(all.aictab, aictab)
         
       }
       
-      
-      # title (paste("Effect of", fixed.v), outer = T, line = 1)
-      # mtext(side = 1, line = 3, text = fixed.v, outer = T)
-      # mtext(side = 2, line = 3,  text = expression("Mg C"~ha^-1~yr^-1), outer = T) 
-      # dev.off()
     }
     
     }
