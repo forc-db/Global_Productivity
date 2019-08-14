8d5415d6797f0c7a71c88dc561d069b099f9c894
beckybanbury <43652200+beckybanbury@users.noreply.github.com>
2019-08-14 16:35:31 -0400

    Create extract_climate_data_growing_season.R

---
+++ scripts/extract_climate_data_growing_season.R
@@ -0,0 +1,116 @@
+##### extract WorldClim and CRU data for ForC_simplified
+
+rm(list = ls())
+library(raster)
+library(ncdf4)
+library(Hmisc)
+library(progress)
+
+ForC_simplified <- read.csv("C:/Users/banburymorganr/Dropbox (Smithsonian)/GitHub/ForC/ForC_simplified/ForC_simplified.csv")
+
+ForC_simplified <- ForC_simplified[!is.na(ForC_simplified$lat),]
+ForC_simplified <- ForC_simplified[!is.na(ForC_simplified$lon),]
+coordinates(ForC_simplified)<-c("lon", "lat")
+proj<-CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+proj4string(ForC_simplified)<-proj
+plot(ForC_simplified)
+as.data.frame(ForC_simplified)
+
+# frs
+r <- brick("S:/Global Maps Data/CRU/v3.23/ncfiles/cru_ts3.23.1901.2014.frs.dat.nc", varname="frs")
+frs.1901.2014 <- raster::extract(r, ForC_simplified)
+
+frs <- data.frame(ForC_simplified)
+frs <- data.frame(measurement.ID = frs[,1], sites.sitename = as.character(frs[,2]), plot.name = as.character(frs[,3]), frs.1901.2014)
+
+frs_colnames <- colnames(frs)[(4:1371)]
+frs_colnames <- gsub("[a-zA-Z ]", "", frs_colnames)
+frs_colnames <- as.Date(frs_colnames, format = "%Y.%m.%d")
+frs_colnames <- monthDays(frs_colnames)
+
+base <- data.frame(measurement.ID = frs[,1], sites.sitename = as.character(frs[,2]), plot.name = as.character(frs[,3]))
+base$sites.sitename <- as.character(base$sites.sitename)
+base$plot.name <- as.character(base$plot.name)
+months <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+years <- c(1901:2014)
+
+subset.all <- NULL
+
+for(year in years){
+  subset <- frs[,grepl(year, colnames(frs))]
+  if(year == 1901) subset.all <- subset
+  subset.all <- cbind(subset, subset.all)
+}
+
+frs_month <- cbind(base, subset.all)
+
+for(month in months){
+  month_frame <- frs_month[,grep(paste0("\\.", month, "\\."), colnames(frs_month))]
+  month_frame$mean <- rowSums(month_frame)
+  month_frame$mean <- month_frame$mean/114
+  colnames(month_frame)[colnames(month_frame)=="mean"] <- month
+  base <- cbind(base, month_frame[, month])
+}
+
+names(base)[4:15] <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+frs_by_month <- base
+
+months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+for(month in months){
+  month_frame <- frs_by_month[,paste(month)]
+  month_frame <- ifelse(month_frame < 1, TRUE, FALSE)
+  base <- cbind(base, month_frame)
+}
+
+names(base)[16:27] <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+frs_months <- base[, c(1:3, 16:27)]
+
+###### monthly average daily minimum temperature
+
+# tmn
+r <- brick("S:/Global Maps Data/CRU/v3.23/ncfiles/cru_ts3.23.1901.2014.tmn.dat.nc", varname="tmn")
+tmn.1901.2014 <- raster::extract(r, ForC_simplified)
+
+tmn <- data.frame(ForC_simplified)
+tmn <- data.frame(measurement.ID = tmn[,1], sites.sitename = as.character(tmn[,2]), plot.name = as.character(tmn[,3]), tmn.1901.2014)
+
+base <- data.frame(measurement.ID = tmn[,1], sites.sitename = as.character(tmn[,2]), plot.name = as.character(tmn[,3]))
+base$sites.sitename <- as.character(base$sites.sitename)
+base$plot.name <- as.character(base$plot.name)
+months <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+years <- c(1901:2014)
+
+subset.all <- NULL
+
+for(year in years){
+  subset <- tmn[,grepl(year, colnames(tmn))]
+  if(year == 1901) subset.all <- subset
+  subset.all <- cbind(subset, subset.all)
+}
+
+tmn_month <- cbind(base, subset.all)
+
+for(month in months){
+  month_frame <- tmn_month[,grep(paste0("\\.", month, "\\."), colnames(tmn_month))]
+  month_frame$mean <- rowSums(month_frame)
+  month_frame$mean <- month_frame$mean/114
+  colnames(month_frame)[colnames(month_frame)=="mean"] <- month
+  base <- cbind(base, month_frame[, month])
+}
+
+names(base)[4:15] <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+tmn_by_month <- base
+
+months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+for(month in months){
+  month_frame <- tmn_by_month[,paste(month)]
+  month_frame <- ifelse(month_frame > 0.5, TRUE, FALSE)
+  base <- cbind(base, month_frame)
+}
+
+names(base)[16:27] <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+tmn_months <- base[, c(1:3, 16:27)]
