d4ea43e8f0a0f3e27be6c3e0c1103306e3d8adf9
beckybanbury <43652200+beckybanbury@users.noreply.github.com>
2019-08-15 16:06:35 -0400

    Update extract_climate_data_growing_season.R

--- scripts/extract_climate_data_growing_season.R
+++ scripts/extract_climate_data_growing_season.R
@@ -5,6 +5,7 @@ library(raster)
 library(ncdf4)
 library(Hmisc)
 library(progress)
+library(dplyr)
 
 ForC_simplified <- read.csv("C:/Users/banburymorganr/Dropbox (Smithsonian)/GitHub/ForC/ForC_simplified/ForC_simplified.csv")
 
@@ -15,6 +16,7 @@ proj<-CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
 proj4string(ForC_simplified)<-proj
 plot(ForC_simplified)
 as.data.frame(ForC_simplified)
+ForC <- as.data.frame(ForC_simplified)
 
 # frs
 r <- brick("S:/Global Maps Data/CRU/v3.23/ncfiles/cru_ts3.23.1901.2014.frs.dat.nc", varname="frs")
@@ -112,14 +114,18 @@ tmn_months <- base[, c(1:3, 16:27)]
 
 ########## mean annual temperature
 
+climate <- c("cld", "pet", "pre", "tmp", "vap")
+
+for(clim in climate){
+
 # tmp
-r <- brick("S:/Global Maps Data/CRU/v3.23/ncfiles/cru_ts3.23.1901.2014.tmp.dat.nc", varname="tmp")
-tmp.1901.2014 <- raster::extract(r, ForC_simplified)
+r <- brick(paste0("S:/Global Maps Data/CRU/v3.23/ncfiles/cru_ts3.23.1901.2014.", clim, ".dat.nc"), varname= clim)
+var.1901.2014 <- raster::extract(r, ForC_simplified)
 
-tmp <- data.frame(ForC_simplified)
-tmp <- data.frame(measurement.ID = tmp[,1], sites.sitename = as.character(tmp[,2]), plot.name = as.character(tmp[,3]), tmp.1901.2014)
+var <- data.frame(ForC_simplified)
+var <- data.frame(measurement.ID = var[,1], sites.sitename = as.character(var[,2]), plot.name = as.character(var[,3]), var.1901.2014)
 
-base <- data.frame(measurement.ID = tmp[,1], sites.sitename = as.character(tmp[,2]), plot.name = as.character(tmp[,3]))
+base <- data.frame(measurement.ID = var[,1], sites.sitename = as.character(var[,2]), plot.name = as.character(var[,3]))
 base$sites.sitename <- as.character(base$sites.sitename)
 base$plot.name <- as.character(base$plot.name)
 months <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
@@ -128,15 +134,15 @@ years <- c(1901:2014)
 subset.all <- NULL
 
 for(year in years){
-  subset <- tmp[,grepl(year, colnames(tmp))]
+  subset <- var[,grepl(year, colnames(var))]
   if(year == 1901) subset.all <- subset
   subset.all <- cbind(subset, subset.all)
 }
 
-tmp_month <- cbind(base, subset.all)
+var_month <- cbind(base, subset.all)
 
 for(month in months){
-  month_frame <- tmp_month[,grep(paste0("\\.", month, "\\."), colnames(tmp_month))]
+  month_frame <- var_month[,grep(paste0("\\.", month, "\\."), colnames(var_month))]
   month_frame$mean <- rowSums(month_frame)
   month_frame$mean <- month_frame$mean/114
   colnames(month_frame)[colnames(month_frame)=="mean"] <- month
@@ -144,10 +150,26 @@ for(month in months){
 }
 
 names(base)[4:15] <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
-tmp_by_month <- base
+var_by_month <- base
 
 ########## merge datasets
 
-tmp <- data.frame(tmn_months, tmp_by_month[, c(4:15)])
+var <- data.frame(tmn_months, var_by_month[, c(4:15)])
 
-                  
+months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+for(month in months){
+x <- ifelse(var[, month] == TRUE, var[, paste0(month, ".1")], NA)
+var <- cbind(var, x)
+}
+
+var <- var[, c(28:39)]
+names(var) <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+var$mean <- rowMeans(var, na.rm = TRUE)
+
+ForC <- cbind(ForC, var$mean)
+}
+
+names(ForC)[35:39] <- c("cld", "pet", "pre", "tmp", "vap")                
+
+write.csv(ForC, "C:/Users/banburymorganr/Dropbox (Smithsonian)/GitHub/ForC/ForC_simplified/ForC_simplified_growing_season_climate.csv", row.names = F)
