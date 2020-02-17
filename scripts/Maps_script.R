96aec3a08ca7959054f0b9cb94b9ec50c852c60f
beckybanbury <becky.bm@hotmail.co.uk>
2020-02-17 10:46:13 +0000

    Update Maps_script.R

--- scripts/Maps_script.R
+++ scripts/Maps_script.R
@@ -4,7 +4,7 @@
 rm(list = ls())
 
 # Set working directory as ForC main folder ####
-setwd("C:/Users/gyrcbm/Dropbox/ForC")
+setwd("C:/Users/becky/Dropbox (Smithsonian)/GitHub/ForC")
 
 # Load libaries ####
 library(lme4)
@@ -122,7 +122,7 @@ ForC_simplified <- ForC_simplified[ages.to.keep & fixed.no.na, ]
 lon <- ForC_simplified$lon
 lat <- ForC_simplified$lat
 
-col.sym <- read.csv("C:/Users/gyrcbm/Dropbox/Global_Productivity/raw.data/colsym.csv", stringsAsFactors = F)
+col.sym <- read.csv("C:/Users/becky/Dropbox (Smithsonian)/GitHub/Global_Productivity/raw.data/colsym.csv", stringsAsFactors = F)
 
 ForC_simplified <- merge(ForC_simplified, col.sym[, c("variable.name", "col", "sym")], by = "variable.name")
 ForC_simplified$col <- as.character(ForC_simplified$col)
@@ -130,12 +130,12 @@ ForC_simplified$sym <- as.character(ForC_simplified$sym)
 
 ForC_simplified$variable.name <- factor(ForC_simplified$variable.name, levels = c("GPP", "NPP", "ANPP", "ANPP_woody_stem", "ANPP_foliage", "BNPP_root", "BNPP_root_fine", "R_auto", "R_auto_root"))
 
-png(file = paste0("C:/Users/gyrcbm/Dropbox/Global_Productivity/results/figures/final_figures/maps/distribution_all_samples.png"), width = 2255, height = 2000, units = "px", res = 300)
+png(file = paste0("C:/Users/becky/Dropbox (Smithsonian)/GitHub/Global_Productivity/results/figures/final_figures/maps/distribution_all_samples.png"), width = 2000, height = 3000, units = "px", res = 300)
 
 mapWorld <- borders("world", colour="gray50", fill="gray50")
 
 mp <- ggplot(data = ForC_simplified) + mapWorld + aes(x=lon, y=lat)
-mp <- mp+ geom_point(aes(color = variable.name, shape = variable.name), size=1) + facet_wrap(vars(variable.name))
+mp <- mp+ geom_point(aes(color = variable.name, shape = variable.name), size=1) + facet_wrap(vars(variable.name), ncol = 2) + theme(legend.position = "none")
 mp <- mp + scale_color_manual(values = plasma(10)[c(1,3,5,7,9,8,6,4,2)]) + scale_shape_manual(values = c(1:9)[c(1,3,5,7,9,8,6,4,2)])  + labs(color = "Variable", shape = "Variable")
                   
 print(mp)
@@ -147,7 +147,7 @@ dev.off()
 mapWorld <- borders("world", colour="gray50", fill="gray50")
 mp <- ggplot(data = ForC_simplified) +   mapWorld
 mp <- mp + geom_point(aes(x=lon, y=lat, shape = variable.name, color = variable.name), size=2) + scale_color_manual(values = plasma(10)[c(1,3,5,7,9,8,6,4,2)]) + scale_shape_manual(values = c(1:9)[c(1,3,5,7,9,8,6,4,2)]) + labs(color = "Variable", shape = "Variable") + theme(axis.title.x = element_blank(), axis.title.y = element_blank(), legend.position = c(0.1, 0.35), legend.text = element_text(size = 12))
-ggsave("C:/Users/gyrcbm/Dropbox/Global_Productivity/results/figures/final_figures/maps/distribution_all_variables.png", plot = mp, width = 14, height = 7)
+ggsave("C:/Users/becky/Dropbox (Smithsonian)/GitHub/Global_Productivity/results/figures/final_figures/maps/distribution_all_variables.png", plot = mp, width = 14, height = 7)
 
 
 
@@ -159,63 +159,63 @@ ggsave("C:/Users/gyrcbm/Dropbox/Global_Productivity/results/figures/final_figure
 # 
 # ### mature forests only ####
 # for(response.variables in response.variables.groups){
-#   
+# 
 #   if(response.variables[1] == "GPP") n <- 1
 #   if(response.variables[1] == "ANPP_1") n <- 2
 #   if(response.variables[1] == "ANPP_woody") n <- 3
 #   if(response.variables[1] == "woody.mortality_ag") n <- 4
-#   
+# 
 #   for (age in ages){
-#     
+# 
 #     if (age %in% "age.greater.than.100") ages.to.keep <- ForC_simplified$stand.age >= 100 & !is.na(ForC_simplified$stand.age)
 #     if (age %in% "age.greater.than.200") ages.to.keep <- ForC_simplified$stand.age >= 200 & !is.na(ForC_simplified$stand.age)
-#     
+# 
 #     for(fixed.v in fixed.variables){
-#       
-#       # tiff(file = paste0("C:/Users/gyrcbm/Dropbox/Global_Productivity/results/figures/test/best_model/Effect_of_", fixed.v, "_MATURE_only_", age, "_", n, ".tiff"), width = 2255, height = 2000, units = "px", res = 300)
-#       
+# 
+#       # tiff(file = paste0("C:/Users/becky/Dropbox (Smithsonian)/GitHub/Global_Productivity/results/figures/test/best_model/Effect_of_", fixed.v, "_MATURE_only_", age, "_", n, ".tiff"), width = 2255, height = 2000, units = "px", res = 300)
+# 
 #       print(fixed.v)
-#       
-#       
-#       
+# 
+# 
+# 
 #       ###subset ForC
-#       
-#       
+# 
+# 
 #       first.plot <- TRUE
-#       
+# 
 #       for (response.v in response.variables){
-#         
+# 
 #         if(response.v %in% "NPP") responses.to.keep  <- c("NPP_1")
 #         if(response.v %in% "ANPP") responses.to.keep  <- c("ANPP_1")
 #         if(response.v %in% "ANPP_litterfall") responses.to.keep  <- c("ANPP_litterfall_1")
 #         if(!response.v %in% c("NPP", "ANPP", "ANPP_litterfall")) responses.to.keep  <- response.v
-#         
-#         
+# 
+# 
 #         rows.with.response <- ForC_simplified$variable.name %in% responses.to.keep
-#         
+# 
 #         fixed.no.na <- !is.na(ForC_simplified[, fixed.v])
-#         
+# 
 #         df <- ForC_simplified[rows.with.response & ages.to.keep & fixed.no.na, ]
-#         
+# 
 #         df$fixed <- df[, fixed.v]
-#         
+# 
 #         lon <- df$lon
 #         lat <- df$lat
-#         # 
-#         tiff(file = paste0("C:/Users/gyrcbm/Dropbox/Global_Productivity/results/figures/maps/distribution_", response.v, "_sample.tiff"), width = 2255, height = 2000, units = "px", res = 300)
-#         
+#         #
+#         tiff(file = paste0("C:/Users/becky/Dropbox (Smithsonian)/GitHub/Global_Productivity/results/figures/maps/distribution_", response.v, "_sample.tiff"), width = 2255, height = 2000, units = "px", res = 300)
+# 
 #         mapWorld <- borders("world", colour="gray50", fill="gray50")
 #         mp <- ggplot() +   mapWorld
 #         mp <- mp+ geom_point(aes(x=lon, y=lat, color= variable.name), size=1) + ggtitle(paste0(response.v))
 #         print(mp)
-#         
+# 
 #         dev.off()
-#         
+# 
 #       }
-#       
+# 
 #     }
-#     
+# 
 #   }
 # }
 # 
-# 
+
