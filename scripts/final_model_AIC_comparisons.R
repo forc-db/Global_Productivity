349ef1761e3ef0bbc52982601d417e2c630d72e0
Becky Banbury Morgan <becky.bm@hotmail.co.uk>
2021-02-25 19:25:14 +0000

    Update final_model_AIC_comparisons.R
    tidied, final version

--- scripts/final_model_AIC_comparisons.R
+++ scripts/final_model_AIC_comparisons.R
@@ -4,7 +4,7 @@
 rm(list = ls())
 
 # Set working directory as ForC main folder ####
-setwd("C:/Users/banburymorganr/Dropbox (Smithsonian)/GitHub/ForC")
+setwd("")
 
 # Load libaries ####
 library(lme4)
@@ -14,12 +14,6 @@ library(piecewiseSEM)
 
 # Load data ####
 ForC_simplified <- read.csv("ForC_simplified/ForC_simplified_WorldClim_CRU_refined.csv", stringsAsFactors = F)
-VARIABLES <- read.csv(paste0(dirname(getwd()), "/ForC/data/ForC_variables.csv"), stringsAsFactors = F)
-
-na_codes <- c("NA", "NI", "NRA", "NaN", "NAC") 
-my_is.na <- function(x) { is.na(x) | x %in% na_codes}
-my_na.omit <- function(x) { return(x[!my_is.na(x)])}
-
 
 # Prepare data ####
 
@@ -42,63 +36,21 @@ ForC_simplified$SolarRadiation <- ForC_simplified$SolarRadiation/1000
 
 # Control for some factors ####
 
-
-## keep all ages except na, 0 and 999
-ages.not.999.nor.0.nor.na <- !ForC_simplified$stand.age %in% 999 &  !ForC_simplified$stand.age %in% 0 & !is.na(ForC_simplified$stand.age)
-
 ## Keep only age >=100 (or 999)
 age.greater.than.100 <- ForC_simplified$stand.age >= 100 & !is.na(ForC_simplified$stand.age)
-age.greater.than.200 <- ForC_simplified$stand.age >= 200 & !is.na(ForC_simplified$stand.age)
 ages <- c("age.greater.than.100")
 
 ## keep only stands that are NOT too strongly influence by management/ disturbance
 
 dist.to.keep <- ForC_simplified$managed %in% 0 & ForC_simplified$disturbed %in% 0
 
-## keep only records with min.dbh <= 10cm
-min.dbh.to.keep <- ForC_simplified$min.dbh <= 10 ##& !is.na(ForC_simplified$min.dbh) # this doesn work great, not enough data, but maybe we can just keep the NAs?
-min.dbh.to.keep <- rep(TRUE, nrow(ForC_simplified))
-
 ages.to.keep <- ForC_simplified$stand.age >= 100 & !is.na(ForC_simplified$stand.age)
-
-
 fixed.no.na <- !is.na(ForC_simplified[, "map"]) & !is.na(ForC_simplified[, "masl"]) & !is.na(ForC_simplified[, "mat"])
-
-
 ForC_simplified <- ForC_simplified[dist.to.keep & fixed.no.na & ages.to.keep, ]
 
-## give leaf type
-broadleaf_codes <- c("2TEB", "2TDB", "2TB")
-needleleaf_codes <- c("2TEN", "2TDN", "2TN")
-mixed <- c("2TD", "2TE", "2TM", "2TREE")
-
-ForC_simplified$leaf.type <- ifelse(ForC_simplified$dominant.veg %in% broadleaf_codes, "broadleaf",
-                                    ifelse(ForC_simplified$dominant.veg %in% needleleaf_codes, "needleleaf",
-                                           ifelse(ForC_simplified$dominant.veg %in% mixed, "mixed", "Other")))
-
-## give leaf phenology
-evergreen_codes <- c("2TE", "2TEB", "2TEN")
-deciduous_codes <- c("2TDN", "2TDB", "2TD")
-
-
-ForC_simplified$leaf.phenology <- ifelse(ForC_simplified$dominant.veg %in% evergreen_codes, "evergreen",
-                                         ifelse(ForC_simplified$dominant.veg %in% deciduous_codes, "deciduous", "Other"))
 
 ### exclude Tura due to extreme high latitude
 ForC_simplified <- ForC_simplified[!(ForC_simplified$sites.sitename %in% "Tura"),]
-
-# Prepare some variables ####
-
-## response variable list (fluxes) ####
-all.response.variables <- VARIABLES[c(4, 7:18, 25:32, 37:38, 51:52),]$variable.name
-all.response.variables <- gsub("(_OM|_C)", "", all.response.variables)
-all.response.variables <- gsub("(_0|_1|_2|_3|_4|_5)", "", all.response.variables)
-all.response.variables <- all.response.variables[all.response.variables %in% ForC_simplified$variable.name]
-all.response.variables <- unique(gsub("_\\d", "", all.response.variables))
-
-
-all.response.variables[!all.response.variables %in% unlist(response.variables.groups)]
-
 
 ## prepare results table
 
@@ -126,16 +78,6 @@ for (response.v in response.variables){
   #create all combinations of random / fixed effects
   
   response <- "mean"
-  
-  # uni_comb <-
-  #   unlist( sapply( seq_len(length(1)), 
-  #                   function(x) {
-  #                     apply( combn(effects, 3), 2, function(x) paste(x, collapse = "+"))
-  #                   }))
-  # 
-  # uni_comb <- expand.grid(response, uni_comb) 
-  # uni_comb <- uni_comb[grepl("geographic", uni_comb$Var2), ] #keep random effect
-  # uni_comb <- uni_comb[grepl("masl", uni_comb$Var2), ] #keep masl
   
   comb <-
     unlist( sapply( seq_len(length(1)), 
@@ -179,4 +121,4 @@ for (response.v in response.variables){
   
 }
 
-write.csv(all.results, file = "C:/Users/banburymorganr/Dropbox (Smithsonian)/GitHub/Global_Productivity/results/tables/best_model_outputs/AIC_table.csv", row.names = F)
+write.csv(all.results, file = "AIC_table.csv", row.names = F)
