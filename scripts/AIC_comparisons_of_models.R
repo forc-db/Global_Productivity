36c1fd74a8d2ee8c9cef094a32d0d8821b336c0a
Becky Banbury Morgan <becky.bm@hotmail.co.uk>
2021-02-25 18:53:15 +0000

    final script AIC comparisons

--- scripts/AIC_comparisons_of_models.R
+++ scripts/AIC_comparisons_of_models.R
@@ -3,7 +3,7 @@
 rm(list = ls())
 
 # Set working directory as ForC main folder ####
-setwd("C:/Users/becky/Dropbox (Smithsonian)/GitHub/ForC")
+setwd("")
 
 # Load libaries ####
 library(lme4)
@@ -19,14 +19,6 @@ library(rlist)
 
 # Load data ####
 ForC_simplified <- read.csv("ForC_simplified/ForC_simplified_WorldClim_CRU_refined.csv", stringsAsFactors = F)
-
-VARIABLES <- read.csv(paste0(dirname(getwd()), "/ForC/data/ForC_variables.csv"), stringsAsFactors = F)
-
-
-na_codes <- c("NA", "NI", "NRA", "NaN", "NAC") 
-my_is.na <- function(x) { is.na(x) | x %in% na_codes}
-my_na.omit <- function(x) { return(x[!my_is.na(x)])}
-
 
 # Prepare data ####
 
@@ -48,8 +40,6 @@ ForC_simplified$site_plot <- paste0(ForC_simplified$sites.sitename," ", ForC_sim
 
 
 # Control for some factors ####
-
-
 ## keep all ages except na, 0 and 999
 ages.not.999.nor.0.nor.na <- !ForC_simplified$stand.age %in% 999 &  !ForC_simplified$stand.age %in% 0 & !is.na(ForC_simplified$stand.age)
 
@@ -59,59 +49,16 @@ age.greater.than.200 <- ForC_simplified$stand.age >= 200 & !is.na(ForC_simplifie
 ages <- c("age.greater.than.100")
 
 ## keep only stands that are NOT too strongly influence by management/ disturbance
-
 dist.to.keep <- ForC_simplified$managed %in% 0 & ForC_simplified$disturbed %in% 0
 ForC_simplified <- ForC_simplified[dist.to.keep, ]
-
-## keep only records with min.dbh <= 10cm ## commented out because no values with min.dbh > 10; comment in with future merges if this changes
-# ForC_simplified$min.dbh <- as.numeric(ForC_simplified$min.dbh)
-# # min.dbh.to.keep <- ForC_simplified$min.dbh <= 10 & is.na(ForC_simplified$min.dbh) # this doesn work great, not enough data, but maybe we can just keep the NAs?
-# 
-# min.dbh.to.remove <- ForC_simplified$min.dbh >= 10 & !is.na(ForC_simplified$min.dbh)
-# ForC_simplified <- ForC_simplified[-min.dbh.to.remove, ]
-
-###keep only altitude below 500 metres
-# 
-# alt.to.keep <- ForC_simplified$masl <= 500 & !is.na(ForC_simplified$masl)
-# ForC_simplified <- ForC_simplified[alt.to.keep, ]
-
-## give leaf type
-broadleaf_codes <- c("2TEB", "2TDB", "2TB")
-needleleaf_codes <- c("2TEN", "2TDN", "2TN")
-mixed <- c("2TD", "2TE", "2TM", "2TREE")
-
-ForC_simplified$leaf.type <- ifelse(ForC_simplified$dominant.veg %in% broadleaf_codes, "broadleaf",
-                                    ifelse(ForC_simplified$dominant.veg %in% needleleaf_codes, "needleleaf",
-                                           ifelse(ForC_simplified$dominant.veg %in% mixed, "mixed", "Other")))
-
-## give leaf phenology
-evergreen_codes <- c("2TE", "2TEB", "2TEN")
-deciduous_codes <- c("2TDN", "2TDB", "2TD")
-
-
-ForC_simplified$leaf.phenology <- ifelse(ForC_simplified$dominant.veg %in% evergreen_codes, "evergreen",
-                                         ifelse(ForC_simplified$dominant.veg %in% deciduous_codes, "deciduous", "Other"))
 
 ### exclude Tura due to extreme high latitude
 ForC_simplified <- ForC_simplified[!(ForC_simplified$sites.sitename %in% "Tura"),]
 
-# Prepare some variables ####
-
-## response variable list (fluxes) ####
-all.response.variables <- VARIABLES[c(4, 7:18, 25:32, 37:38, 51:52),]$variable.name
-all.response.variables <- gsub("(_OM|_C)", "", all.response.variables)
-all.response.variables <- gsub("(_0|_1|_2|_3|_4|_5)", "", all.response.variables)
-all.response.variables <- all.response.variables[all.response.variables %in% ForC_simplified$variable.name]
-all.response.variables <- unique(gsub("_\\d", "", all.response.variables))
-
-
 ## prepare results table
-
 all.results <- NULL
 
 ###comparing models
-
-
 all.results <- NULL
 all.aictab <- NULL
 all.koeppen <- NULL
@@ -259,7 +206,7 @@ for(response.variables in response.variables.groups){
        aictab.best$flux <- paste(response.v)
        aictab.top <- rbind(aictab.top, aictab.best)
 
-      write.csv(aictab.list, file = paste0("C:/Users/becky/Dropbox (Smithsonian)/GitHub/Global_Productivity/results/tables/best_model_outputs/best_models_AIC/", response.v, "_AIC.csv"))
+      write.csv(aictab.list, file = "")
       }
       
       
@@ -271,4 +218,4 @@ for(response.variables in response.variables.groups){
 }
 
 
-write.csv(aictab.top, file = "C:/Users/becky/Dropbox (Smithsonian)/GitHub/Global_Productivity/results/tables/best_model_outputs/best_models_AIC/top_aic.csv")
+write.csv(aictab.top, file = "")
