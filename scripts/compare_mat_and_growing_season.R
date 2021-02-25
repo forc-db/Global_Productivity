b8e36b62c94059a0403950f89cf52b7fb4315c1e
Becky Banbury Morgan <becky.bm@hotmail.co.uk>
2021-02-25 18:57:55 +0000

    final code for mat + LGS comparison and correlation matrix code

--- scripts/compare_mat_and_growing_season.R
+++ scripts/compare_mat_and_growing_season.R
@@ -4,7 +4,7 @@
 rm(list = ls())
 
 # Set working directory as ForC main folder ####
-setwd("C:/Users/becky/Dropbox (Smithsonian)/GitHub/ForC")
+setwd("")
 
 # Load libaries ####
 library(lme4)
@@ -14,12 +14,6 @@ library(piecewiseSEM)
 
 # Load data ####
 ForC_simplified <- read.csv("ForC_simplified/ForC_simplified_WorldClim_CRU_refined.csv", stringsAsFactors = F)
-VARIABLES <- read.csv(paste0(dirname(getwd()), "/ForC/data/ForC_variables.csv"), stringsAsFactors = F)
-
-na_codes <- c("NA", "NI", "NRA", "NaN", "NAC") 
-my_is.na <- function(x) { is.na(x) | x %in% na_codes}
-my_na.omit <- function(x) { return(x[!my_is.na(x)])}
-
 
 # Prepare data ####
 
@@ -39,16 +33,10 @@ ForC_simplified$lat <- abs(ForC_simplified$lat)
 
 ForC_simplified$SolarRadiation <- ForC_simplified$SolarRadiation/1000
 
-
 # Control for some factors ####
-
-
-## keep all ages except na, 0 and 999
-ages.not.999.nor.0.nor.na <- !ForC_simplified$stand.age %in% 999 &  !ForC_simplified$stand.age %in% 0 & !is.na(ForC_simplified$stand.age)
 
 ## Keep only age >=100 (or 999)
 age.greater.than.100 <- ForC_simplified$stand.age >= 100 & !is.na(ForC_simplified$stand.age)
-age.greater.than.200 <- ForC_simplified$stand.age >= 200 & !is.na(ForC_simplified$stand.age)
 ages <- c("age.greater.than.100")
 
 ## keep only stands that are NOT too strongly influence by management/ disturbance
@@ -61,44 +49,12 @@ min.dbh.to.keep <- rep(TRUE, nrow(ForC_simplified))
 
 ages.to.keep <- ForC_simplified$stand.age >= 100 & !is.na(ForC_simplified$stand.age)
 
-
 fixed.no.na <- !is.na(ForC_simplified[, "map"]) & !is.na(ForC_simplified[, "masl"]) & !is.na(ForC_simplified[, "mat"])
-
 
 ForC_simplified <- ForC_simplified[dist.to.keep & fixed.no.na & ages.to.keep, ]
 
-## give leaf type
-broadleaf_codes <- c("2TEB", "2TDB", "2TB")
-needleleaf_codes <- c("2TEN", "2TDN", "2TN")
-mixed <- c("2TD", "2TE", "2TM", "2TREE")
-
-ForC_simplified$leaf.type <- ifelse(ForC_simplified$dominant.veg %in% broadleaf_codes, "broadleaf",
-                                    ifelse(ForC_simplified$dominant.veg %in% needleleaf_codes, "needleleaf",
-                                           ifelse(ForC_simplified$dominant.veg %in% mixed, "mixed", "Other")))
-
-## give leaf phenology
-evergreen_codes <- c("2TE", "2TEB", "2TEN")
-deciduous_codes <- c("2TDN", "2TDB", "2TD")
-
-
-ForC_simplified$leaf.phenology <- ifelse(ForC_simplified$dominant.veg %in% evergreen_codes, "evergreen",
-                                         ifelse(ForC_simplified$dominant.veg %in% deciduous_codes, "deciduous", "Other"))
-
 ### exclude Tura due to extreme high latitude
 ForC_simplified <- ForC_simplified[!(ForC_simplified$sites.sitename %in% "Tura"),]
-
-# Prepare some variables ####
-
-## response variable list (fluxes) ####
-all.response.variables <- VARIABLES[c(4, 7:18, 25:32, 37:38, 51:52),]$variable.name
-all.response.variables <- gsub("(_OM|_C)", "", all.response.variables)
-all.response.variables <- gsub("(_0|_1|_2|_3|_4|_5)", "", all.response.variables)
-all.response.variables <- all.response.variables[all.response.variables %in% ForC_simplified$variable.name]
-all.response.variables <- unique(gsub("_\\d", "", all.response.variables))
-
-
-all.response.variables[!all.response.variables %in% unlist(response.variables.groups)]
-
 
 ## prepare results table
 
@@ -138,9 +94,8 @@ for (response.v in response.variables){
   aictab <- cbind(aictab, Rsq)
   
   all.aictab <- rbind(all.aictab, aictab)
-  # all.results <- all.results <- rbind(all.results, results)
- 
+
   
 }
 
-write.csv(all.aictab, file = "C:/Users/becky/Dropbox (Smithsonian)/GitHub/Global_Productivity/results/tables/best_model_outputs/AIC_table_mat_growing_season.csv", row.names = F)
+write.csv(all.aictab, file = "", row.names = F)
