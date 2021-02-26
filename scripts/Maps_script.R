be12c39a1067a38b0f6ae99dcc660364d86011a8
Becky Banbury Morgan <becky.bm@hotmail.co.uk>
2021-02-26 14:17:33 +0000

    maps update

--- scripts/Maps_script.R
+++ scripts/Maps_script.R
@@ -4,21 +4,17 @@
 rm(list = ls())
 
 # Set working directory as ForC main folder ####
-setwd("C:/Users/becky/Dropbox (Smithsonian)/GitHub/ForC")
+setwd("C:/Users/gyrcbm/Documents/GitHub/Global_Productivity")
 
 # Load libaries ####
 library(lme4)
 library(MuMIn)
 library(ggplot2)
 library(viridis)
+library(maps)
 
 # Load data ####
 ForC_simplified <- read.csv("ForC_simplified/ForC_simplified_WorldClim_CRU_refined.csv", stringsAsFactors = F)
-VARIABLES <- read.csv(paste0(dirname(getwd()), "/ForC/data/ForC_variables.csv"), stringsAsFactors = F)
-
-na_codes <- c("NA", "NI", "NRA", "NaN", "NAC") 
-my_is.na <- function(x) { is.na(x) | x %in% na_codes}
-my_na.omit <- function(x) { return(x[!my_is.na(x)])}
 
 
 # Prepare data ####
@@ -46,7 +42,6 @@ ages.not.999.nor.0.nor.na <- !ForC_simplified$stand.age %in% 999 &  !ForC_simpli
 
 ## Keep only age >=100 (or 999)
 age.greater.than.100 <- ForC_simplified$stand.age >= 100 & !is.na(ForC_simplified$stand.age)
-age.greater.than.200 <- ForC_simplified$stand.age >= 200 & !is.na(ForC_simplified$stand.age)
 ages <- c("age.greater.than.100")
 
 ## keep only stands that are NOT too strongly influence by management/ disturbance
@@ -60,39 +55,10 @@ ForC_simplified$min.dbh <- as.numeric(ForC_simplified$min.dbh)
 min.dbh.to.remove <- ForC_simplified$min.dbh >= 10 & !is.na(ForC_simplified$min.dbh)
 ForC_simplified <- ForC_simplified[-min.dbh.to.remove, ]
 
-###keep only altitude below 500 metres
-# 
-# alt.to.keep <- ForC_simplified$masl <= 500 & !is.na(ForC_simplified$masl)
-# ForC_simplified <- ForC_simplified[alt.to.keep, ]
-
-## give leaf type
-broadleaf_codes <- c("2TEB", "2TDB", "2TB")
-needleleaf_codes <- c("2TEN", "2TDN", "2TN")
-mixed <- c("2TD", "2TE", "2TM", "2TREE")
-
-ForC_simplified$leaf.type <- ifelse(ForC_simplified$dominant.veg %in% broadleaf_codes, "broadleaf",
-                                    ifelse(ForC_simplified$dominant.veg %in% needleleaf_codes, "needleleaf",
-                                           ifelse(ForC_simplified$dominant.veg %in% mixed, "mixed", "Other")))
-
-## give leaf phenology
-evergreen_codes <- c("2TE", "2TEB", "2TEN")
-deciduous_codes <- c("2TDN", "2TDB", "2TD")
-
-
-ForC_simplified$leaf.phenology <- ifelse(ForC_simplified$dominant.veg %in% evergreen_codes, "evergreen",
-                                         ifelse(ForC_simplified$dominant.veg %in% deciduous_codes, "deciduous", "Other"))
-
 ### exclude Tura due to extreme high latitude
 ForC_simplified <- ForC_simplified[!(ForC_simplified$sites.sitename %in% "Tura"),]
 
 # Prepare some variables ####
-
-## response variable list (fluxes) ####
-all.response.variables <- VARIABLES[c(4, 7:18, 25:32, 37:38, 51:52),]$variable.name
-all.response.variables <- gsub("(_OM|_C)", "", all.response.variables)
-all.response.variables <- gsub("(_0|_1|_2|_3|_4|_5)", "", all.response.variables)
-all.response.variables <- all.response.variables[all.response.variables %in% ForC_simplified$variable.name]
-all.response.variables <- unique(gsub("_\\d", "", all.response.variables))
 
 ForC_simplified$variable.name[ForC_simplified$variable.name %in% "BNPP_root"] <- "BNPP"
 ForC_simplified$variable.name[ForC_simplified$variable.name %in% "ANPP_foliage"] <- "ANPP foliage"
@@ -129,7 +95,7 @@ ForC_simplified <- ForC_simplified[ages.to.keep & fixed.no.na, ]
 lon <- ForC_simplified$lon
 lat <- ForC_simplified$lat
 
-col.sym <- read.csv("C:/Users/becky/Dropbox (Smithsonian)/GitHub/Global_Productivity/raw.data/colsym.csv", stringsAsFactors = F)
+col.sym <- read.csv("C:/Users/gyrcbm/Documents/GitHub/Global_Productivity/raw.data/colsym.csv", stringsAsFactors = F)
 
 ForC_simplified <- merge(ForC_simplified, col.sym[, c("maps.name", "col", "sym")], by.x = "variable.name", by.y = "maps.name")
 ForC_simplified$col <- as.character(ForC_simplified$col)
@@ -154,7 +120,7 @@ dev.off()
 mapWorld <- borders("world", colour="gray50", fill="gray50")
 mp <- ggplot(data = ForC_simplified) +   mapWorld
 mp <- mp + geom_point(aes(x=lon, y=lat, shape = variable.name, color = variable.name), size=2) + scale_color_manual(values = plasma(10)[c(1,3,5,7,9,8,6,4,2)]) + scale_shape_manual(values = c(1:9)[c(1,3,5,7,9,8,6,4,2)]) + labs(color = "Variable", shape = "Variable") + theme(axis.title.x = element_blank(), axis.title.y = element_blank(), legend.position = c(0.1, 0.35), legend.text = element_text(size = 12))
-ggsave("C:/Users/becky/Dropbox (Smithsonian)/GitHub/Global_Productivity/results/figures/final_figures/maps/distribution_all_variables.png", plot = mp, width = 14, height = 7)
+ggsave("C:/Users/gyrcbm/Documents/GitHub/Global_Productivity/results/figures/final_figures/maps/distribution_all_variables.jpg", plot = mp, width = 14, height = 7)
 
 
 
